17166477074e3478fd6c77f2943312ab82140f39
Christian Groothoff <ruffo324@gmail.com>
2018-09-11 05:13:41 +0200

    Makelist update

--- makelist.lua
+++ makelist.lua
@@ -5,6 +5,15 @@ Console.WriteLine(Console.Type.Debug, "I'm working currently on this script.")
 
 -- Write monitor name on every connected monitor
 local peripherals = peripheral.getNames()
-  for i = 1, #peripherals do
-    Console.WriteLine(Console.Type.Debug, "Type: \"" .. peripheral.getType(peripherals[i]) .. "\" attached as \"".. peripherals[i] .. "\"")
+for i = 1, #peripherals do
+  if(peripheral.getType(peripherals[i]) == "monitor") then
+    local tempPeripheral = peripheral.wrap(peripherals[i])
+    tempPeripheral.setTextScale(0.5)
+    Console.WriteLine(Console.Type.Hint, peripherals[i], tempPeripheral)      
   end
+end
+
+Console.WriteLine(Console.Type.Debug, "Monitor name on")
+Console.WriteLine(Console.Type.Debug, "each monitor..")
+
+Console.WriteLine(Console.Type.Hint, "Gutenacht ;)")
