fa4f111a3e72f09a01546d243bb1d170606057e4
Christian Groothoff <ruffo324@gmail.com>
2018-09-11 05:10:09 +0200

    Editing makelist

--- makelist.lua
+++ makelist.lua
@@ -1,1 +1,10 @@
 --- This script is used to create a new "monitorHandler.cfg".
+os.loadAPI("/Utils/Utils") -- Load all Utils
+
+Console.WriteLine(Console.Type.Debug, "I'm working currently on this script.")
+
+-- Write monitor name on every connected monitor
+local peripherals = peripheral.getNames()
+  for i = 1, #peripherals do
+    Console.WriteLine(Console.Type.Debug, "Type: \"" .. peripheral.getType(peripherals[i]) .. "\" attached as \"".. peripherals[i] .. "\"")
+  end
