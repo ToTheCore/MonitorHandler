d709098081879b26fdc29a9f9f1dbd6a2df3a3b7
Christian Groothoff <ruffo324@gmail.com>
2018-09-12 22:14:31 +0200

    ToTheCore/Downloader#3 Fixing wrong url, adding silent mode to wget

--- installer.lua
+++ installer.lua
@@ -13,10 +13,10 @@ shell.run("delete", "installer")
 term.setTextColor(colors.green)
 print("Downloading \"" .. thisRepoName .. "\" files..")
 term.setTextColor(colors.gray)
-shell.run("/wget", "https://raw.githubusercontent.com/" .. thisRepoName .. "/master/MonitorHandler.lua","/" .. thisRepoName .. "/MonitorHandler")
-shell.run("/wget", "https://raw.githubusercontent.com/" .. thisRepoName .. "/master/makelist.lua","/" .. thisRepoName .. "/makelist")
+shell.run("/wget", "https://raw.githubusercontent.com/ToTheCore/" .. thisRepoName .. "/master/MonitorHandler.lua","/" .. thisRepoName .. "/MonitorHandler", "-silent")
+shell.run("/wget", "https://raw.githubusercontent.com/ToTheCore/" .. thisRepoName .. "/master/makelist.lua","/" .. thisRepoName .. "/makelist", "-silent")
 
 -- Downloading installer/updater
-shell.run("/wget", "https://raw.githubusercontent.com/" .. thisRepoName .. "/master/updater.lua","/" .. thisRepoName .. "/updater")
-shell.run("/wget", "https://raw.githubusercontent.com/" .. thisRepoName .. "/master/installer.lua","/" .. thisRepoName .. "/installer")
+shell.run("/wget", "https://raw.githubusercontent.com/ToTheCore/" .. thisRepoName .. "/master/updater.lua","/" .. thisRepoName .. "/updater", "-silent")
+shell.run("/wget", "https://raw.githubusercontent.com/ToTheCore/" .. thisRepoName .. "/master/installer.lua","/" .. thisRepoName .. "/installer", "-silent")
 
